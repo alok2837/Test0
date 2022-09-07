ded146e641b85670b572b8ddb3be5bf3736b278b
alok2837 <alok2837@gmail.com>
2022-09-07 07:48:39 +0530

    User can comment and delete

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   }
   get '/u/:id', to: 'users#profile', as: 'user'
 
-  resources :posts
+  # /posts/1/comments/4
+  resources :posts do
+    resources :comments
+  end
+  
   root "pages#home"
   get 'about', to: 'pages#about'
 end
